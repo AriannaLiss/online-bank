ef56b5b968a9d3836b4bf39b84094c10061d1f2a
Lidiia Kalakutska <lidia.kalakutskaya@gmail.com>
2023-05-10 12:47:43 +0200

    Added one more reducer. Reducers combined, added composeWithDevTools into createStore() for controle a state of the store. Refuctored.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useDispatch, useSelector } from "react-redux"
+import './App.css'
 
 const App = () => {
     const dispatch = useDispatch()
-    const cash = useSelector(state => state.cash)
+    const cash = useSelector(state => state.cash.cash)
     console.log(cash)
 
     const getCash = (cash) => {
@@ -15,9 +16,8 @@ const App = () => {
 
     return (
         <div className='app'>
-            Hello world
-            <div style={{fontSize:'3rem'}}>{cash}</div>
-            <div style={{display:'flex'}}>
+            <div className='display'><p>{cash}</p></div>
+            <div className='bottons'>
                 <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
                 <button onClick={() => getCash(Number(prompt()))}>Cнять со счета</button>
             </div>
